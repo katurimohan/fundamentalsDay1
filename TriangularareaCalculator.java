1f27972a079bc43151cafdee09da9042bfae50ec
Katuri Mohan kumar <mohankumarkaturi1@gmail.com>
2025-11-01 12:04:34 +0530

    Add files via upload

---
+++ TriangularareaCalculator.java
@@ -0,0 +1,18 @@
+package com.codegnan.fundamentals;
+
+import java.util.Scanner;
+
+public class TriangularareaCalculator {
+
+	public static void main(String[] args) {
+	 Scanner scanner=new Scanner(System.in);
+	 System.out.println("Enter the base of the triangle : ");
+	 double base=scanner.nextDouble();
+	 System.out.println("Enter Height of the triangle : ");
+	 double height=scanner.nextDouble();
+	 double area=0.5*base*height;
+	 System.out.println("Area of Triangle : "+area);
+	 scanner.close();
+	}
+
+}
